8b2f8a444f9834f94f00b232fa8bb62d6479c951
mkg6573 <mohitgupta6573@gmail.com>
2025-03-21 23:43:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('mohit')
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image('wp9456171.jpg')
+
+with col2:
+    st.write('nk ii i ikgig rbosghg viuehgu bvueh u buvj dujfn ubvjeiuerh vbuvejveuv jk ji ni k')
