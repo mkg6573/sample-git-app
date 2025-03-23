10da2b841a34275f0690b01117cbe47f6d21341c
mkg6573 <mohitgupta6573@gmail.com>
2025-03-23 10:10:12 +0530

    replace header image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title('mohit')
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image('wp9456171.jpg')
+    st.image('aa.jpg')
 
 with col2:
     st.write('nk ii i ikgig rbosghg viuehgu bvueh u buvj dujfn ubvjeiuerh vbuvejveuv jk ji ni k')
